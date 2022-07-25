bc2a2b4ee60296324aa05df9f742874dd5e0242b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-25 10:44:37 -0300

    update

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import alfy from 'alfy';
+import puppeteer from "puppeteer";
 
 // [INPUT]
-const input = alfy.input;
+const input = alfy.input.trim();
 
 // [PROCESS]
+const browser = await puppeteer.launch({ headless: true });
+const page = await browser.newPage();
+const moedaBase = `${input} dolar`;
+const moedaFinal = 'real';
+const currencyUrl = `https://www.google.com/search?q=${moedaBase}+para+${moedaFinal}&oq=${moedaBase}+para+${moedaFinal}&aqs=chrome.0.69i59j0l7.1726j0j4&sourceid=chrome&ie=UTF-8`;
+await page.goto(currencyUrl);
+const resultado = await page.evaluate(() => {
+    return document.querySelector('.a61j6.vk_gy.vk_sh.Hg3mWc').value;
+});
+await browser.close();
+
+
 const currencies = [
     {
         flag: '🇧🇷',
 		name: 'BRL',
 		prefix: "R$",
-		value: input,
+		value: resultado,
 	},
 	{
         flag: '🇺🇸',
