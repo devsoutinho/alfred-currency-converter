f61cb0e4fcb4e7a0c35518d807db72e6618354e2
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-25 09:42:05 -0300

    update

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const input = alfy.input;
 
 // [PROCESS]
 const currencies = [
-	{
-		flag: '🇧🇷',
+    {
+        flag: '🇧🇷',
 		name: 'BRL',
 		prefix: "R$",
 		value: input,
 	},
 	{
-		flag: '🇺🇸',
+        flag: '🇺🇸',
 		name: 'USD',
 		prefix: "$",
 		value: input,
 	}
 ];
+
+// [OUTPUT]
 const output = currencies.map((currency) => ({
 	title: `${currency.flag} ${currency.prefix}${currency.value} - ${currency.name} `
 }));
-
-// [OUTPUT]
 alfy.output(output);
